eac7d50a2482071cb1b7a9199027534cc127f847
DJthefirst <djberry8@ptd.net>
2021-08-08 17:22:17 -0400

    Works with chars

--- ESP32 Core/src/main.cpp
+++ ESP32 Core/src/main.cpp
@@ -3,9 +3,13 @@
 #include <WiFi.h>
 
 const char* ssid     = "Datanet";  //Network Name
-const char* password = "********"; //Network Password
+const char* password = "******"; //Network Password
 const char* host = "192.168.1.28"; //Device IP
 const int port = 8088;
+byte dataByte = 0;
+
+boolean alreadyConnected = false; // whether or not the client was connected previously
+String inData;
 
 WiFiClient client;
 
@@ -35,10 +39,39 @@ void setup()
   }
 }
 
-void loop()
-{
-  if(client.read() != -1){
-  Serial.println(client.read());
-  client.print(client.read());
+// void loop(){
+//   dataByte = client.read();
+//   Serial.println(dataByte);
+//   client.print(dataByte);
+//   delay(2000);
+// }
+
+void loop() {
+  // when the client sends the first byte, say hello:
+  if (client) {
+    if (!alreadyConnected) {
+      // clead out the input buffer:
+      client.flush();    
+      Serial.println("We have a new client");
+      client.println("Hello, client!"); 
+      alreadyConnected = true;
+    } 
+
+    if (client.available() > 0) { 
+      // read the bytes incoming from the client:
+      char thisChar = client.read();
+      inData += thisChar; 
+      // echo the bytes back to the client:
+      client.write(thisChar);
+      // echo the bytes to the server as well:
+      Serial.write(thisChar);
+    
+      // echo to the server what's been received to confirm we have the string
+      if (thisChar == '\n'){
+        Serial.print("\nreceived:");
+        Serial.print(inData);
+        inData = "";          
+      }
+    }
   }
 }
