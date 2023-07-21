77292d7c34229fac185602ff0d5f8c2975e0ab4d
DJthefirst <yachbascbusb>
2023-07-21 00:03:07 -0400

    Create Build?

--- Chrome_JS_Server_V2.0/my-app/vite.config.ts
+++ Chrome_JS_Server_V2.0/my-app/vite.config.ts
@@ -2,6 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	base: "/Mechanical-Midi-Music/",
 	plugins: [sveltekit()]
 });
